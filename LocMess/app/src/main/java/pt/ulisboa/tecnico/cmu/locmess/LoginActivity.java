b19ba742a87a21df22393245dd986de2a3fed2cb
tiagofbfernandes <tiagofbfernandes@gmail.com>
2017-03-21 11:07:13 +0000

    sign up started

--- LocMess/app/src/main/java/pt/ulisboa/tecnico/cmu/locmess/LoginActivity.java
+++ LocMess/app/src/main/java/pt/ulisboa/tecnico/cmu/locmess/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,6 +13,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button bLogin = (Button) findViewById(R.id.bLogin);
+
+    }
+
+    public void login(View view){
+
     }
 
     public void register(View view){
--- LocMess/app/src/main/java/pt/ulisboa/tecnico/cmu/locmess/RegisterActivity.java
+++ LocMess/app/src/main/java/pt/ulisboa/tecnico/cmu/locmess/RegisterActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -17,6 +19,21 @@ public class RegisterActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etPasswordConfirmation = (EditText) findViewById(R.id.etPasswordConfirmation);
+        final Button bSignup = (Button) findViewById(R.id.bSignup);
     }
+
+
+    private boolean isUsernameValid(String username) {
+        String pattern= "^[a-zA-Z0-9]*$";
+        return username.matches(pattern);
+    }
+
+
+
+
 
 }
