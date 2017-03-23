797f815d58e57e3535bf18c71eb880cb1cc065b0
joaaoleite <joaogsleite@gmail.com>
2017-03-23 13:50:48 +0000

    asd

--- LocMess/app/src/main/java/pt/ulisboa/tecnico/cmu/locmess/MainActivity.java
+++ LocMess/app/src/main/java/pt/ulisboa/tecnico/cmu/locmess/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -14,10 +15,7 @@ import android.support.v4.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
     private Fragment fragment;
-    private FragmentManager fragmentManager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -27,19 +25,18 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_profile:
                     setTitle(getResources().getString(R.string.title_profile));
-                    fragment = new ProfileFragment();
+                    fragment = ProfileFragment.newInstance("test","test");
                     break;
                 case R.id.navigation_locations:
-                    setTitle(getResources().getString(R.string.title_profile));
-                    fragment = new ProfileFragment();
+                    setTitle(getResources().getString(R.string.title_locations));
+                    fragment = LocationsFragment.newInstance("test","test");
                     break;
                 case R.id.navigation_messages:
-                    setTitle(getResources().getString(R.string.title_profile));
-                    fragment = new ProfileFragment();
+                    setTitle(getResources().getString(R.string.title_messages));
+                    fragment = MessagesFragment.newInstance("test","test");
                     break;
             }
-            final FragmentTransaction transaction = fragmentManager.beginTransaction();
-            transaction.replace(R.id.main_container, fragment).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_container, fragment).commit();
             return true;
         }
     };
@@ -49,12 +46,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        fragmentManager = getSupportFragmentManager();
-        navigation.inflateMenu(R.menu.navigation);
+        //navigation.inflateMenu(R.menu.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_container, ProfileFragment.newInstance("","")).commit();
+    }
 }
