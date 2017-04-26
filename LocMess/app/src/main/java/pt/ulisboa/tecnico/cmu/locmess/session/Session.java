8b7f9d96541f152ca3a23aa6c6d48a93165b0632
joaaoleite <joaogsleite@gmail.com>
2017-04-26 01:09:51 +0100

    voley

---
+++ LocMess/app/src/main/java/pt/ulisboa/tecnico/cmu/locmess/session/Session.java
@@ -0,0 +1,114 @@
+package pt.ulisboa.tecnico.cmu.locmess.session;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class Session {
+
+    public static final String APP_NAME = "pt.ulisboa.tecnico.cmu.locmess";
+    public static final String BASE_URL = "http://cmu.n1z.pt:8080";
+
+    private static Session instance = null;
+
+    private RequestQueue queue;
+    private SharedPreferences prefs;
+
+    private Session(Context context, SharedPreferences prefs) {
+        this.queue = Volley.newRequestQueue(context);
+        this.prefs = prefs;
+    }
+    public static Session getInstance(){ return instance; }
+    public static Session getInstance(Activity act) {
+        if(instance==null) {
+            Context context = act.getApplicationContext();
+            SharedPreferences prefs = act.getSharedPreferences(APP_NAME, context.MODE_PRIVATE);
+            instance = new Session(context, prefs);
+            Log.d("Session", "Init");
+        }
+        return instance;
+    }
+
+    // --------------------------------------------
+
+    public void request(StringRequest req){
+        this.queue.add(req);
+    }
+
+    public SharedPreferences.Editor editor(){
+        return prefs.edit();
+    }
+
+    // --------------------------------------------
+
+    public boolean isLoggedIn(){
+        try{
+            return prefs.getBoolean("login", false);
+        }
+        catch (Exception e){
+            return false;
+        }
+    }
+
+    public String token(){
+        if(isLoggedIn())
+            return prefs.getString("token","...");
+        else
+            return null;
+    }
+
+    public void token(String newToken){
+        if(newToken != null){
+            SharedPreferences.Editor editor = editor();
+            editor.clear();
+            editor.putBoolean("login", true);
+            editor.putString("token", newToken);
+            editor.apply();
+        }
+        else logout();
+    }
+
+    // --------------------------------------------
+
+    public void login(String username, String password){
+
+        Log.d("Session", "Login...");
+
+        HashMap<String,String> params = new HashMap<>();
+        params.put("username",username);
+        params.put("password",password);
+
+        new Request("POST","/login",params){
+            public void onResponse(JSONObject res){
+                try{
+                    String token = res.getString("token");
+                    instance.token(token);
+                }catch(JSONException e){
+                    instance.logout();
+                }
+            }
+            public void onError(String error){
+                instance.logout();
+            }
+        }.execute();
+    }
+
+    public void logout(){
+        SharedPreferences.Editor editor = editor();
+        editor.clear();
+        editor.putBoolean("login",false);
+        editor.putString("token",null);
+        editor.apply();
+    }
+}
