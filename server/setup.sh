b10bd835b7c6a19cc193ceb22ebbbd361973d0d8
joaotomazio <jdtomazio@gmail.com>
2017-05-09 00:01:47 +0100

    add https

--- server/setup.sh
+++ server/setup.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install oracle-java8-installer
 sudo apt-get install oracle-java8-set-default
 sudo apt-get install -y maven
-sudo apt-get install -y git
-git clone -b server https://jdtomazio@gitlab.com/joaaoleite/cmu-project.git
-cd cmu-project/server
+cd cmu-project/server/src/main/java/pt/ulisboa/tecnico/cmu/server
+keytool -genkey -alias server -keyalg RSA -keystore keystore.jks -keysize 2048
+cd ~/cmu-project/server
 mvn compile exec:exec
